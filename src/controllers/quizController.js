90e4dadd4b714cd832b3381c6bccaa4aff2e8a12
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-03-17 15:37:00 +0000

    adding create question functionality

--- src/controllers/quizController.js
+++ src/controllers/quizController.js
@@ -1,15 +1,38 @@
-const Question = require('../models/Question.js')
+const Question = require("../models/Question.js");
 
 const getAllQuestions = (req, res) => {
-    let questions = Question.find();
+  let questions = Question.find();
 
-    res.status(200).json(questions)
-}
+  res.status(200).json(questions);
+};
 
 const getQuestionByID = (req, res) => {
-    let question = Question.findById(req.params.id);
+  let question = Question.findById(req.params.id);
 
+  res.status(200).json(question);
+};
+
+const createQuestion = (req, res) => {
+  let { body } = req;
+
+  if (typeof body.question != "string") {
+    res.status(422).json({
+      error: "incorrect input",
+      shape: {
+        question: "Who was the first president of the United States?",
+        choices: [
+          "George Washington",
+          "Thomas Jefferson",
+          "Abraham Lincoln",
+          "John F. Kennedy",
+        ],
+        answer: "George Washington",
+      },
+    });
+  } else {
+    let question = Question.save(body);
     res.status(200).json(question);
-}
+  }
+};
 
-module.exports = { getAllQuestions, getQuestionByID }
+module.exports = { getAllQuestions, getQuestionByID, createQuestion };
